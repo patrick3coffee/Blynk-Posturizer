295eb3ed556bd819ae5bfded359eba8b83a3cc05
patrick3coffee <pwprescott@gmail.com>
2016-06-08 06:11:29 -0600

    working but buggy

--- Blynk_Posturizer.ino
+++ Blynk_Posturizer.ino
@@ -3,9 +3,9 @@
 #include <BlynkSimpleEsp8266.h>
 #include <Adafruit_NeoPixel.h>
 
-////////////////////
-// Blynk Settings //
-////////////////////
+////////////////////    The BlynkAuth, Wifi Name, and Wifi Password are 
+// Blynk Settings //    stored in a separate file and excluded from GitHub
+////////////////////    commits to protect my privacy. 
 #include "secretSettings.h"
 
 ///////////////////////
@@ -13,10 +13,11 @@
 ///////////////////////
 #define WS2812_PIN 4 // Pin connected to WS2812 LED
 #define BUTTON_PIN 0
-#define LED_PIN    5
+#define VIBE_PIN   5
 Adafruit_NeoPixel rgb = Adafruit_NeoPixel(1, WS2812_PIN, NEO_GRB + NEO_KHZ800);
 int sensorThreshold = 512;
-int timeLimit = 10;
+unsigned long timeLimit = 10000;
+unsigned long currentTime,previousRead;
 
 BLYNK_WRITE(V0) // Handle RGB from the zeRGBa
 {
@@ -32,18 +33,8 @@ BLYNK_WRITE(V0) // Handle RGB from the zeRGBa
   rgb.show();
 }
 
-bool checkToggle()
-{
-  if(digitalRead(12) == HIGH){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
 BLYNK_WRITE(V8){
-  sensorThreshold = param.asInt();
+  sensorThreshold = param.asInt() * 1000;
 }
 
 BLYNK_WRITE(V9){
@@ -58,7 +49,7 @@ void setup()
 #endif
   rgb.begin(); // RGB LED
   pinMode(BUTTON_PIN, INPUT); // Button input
-  pinMode(LED_PIN, OUTPUT); // LED output
+  pinMode(VIBE_PIN, OUTPUT); // Vibrator output
 
   // Initialize Blynk
   Blynk.begin(BlynkAuth, WiFiNetwork, WiFiPassword);
@@ -68,5 +59,43 @@ void loop()
 {
   // Execute Blynk.run() as often as possible during the loop
   Blynk.run(); 
+  verify();
 }
+
+void verify(){
+  currentTime = millis();
+  readSensor();
+  if(!checkTime()){
+    enforce();
+  };
+}
+
+bool readSensor()
+{
+  if(analogRead(A0) > sensorThreshold){
+    previousRead = currentTime;
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+bool checkTime(){
+  if(currentTime - previousRead < timeLimit){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+void enforce(){
+  digitalWrite(VIBE_PIN, HIGH);
+  delay(200);
+  digitalWrite(VIBE_PIN, LOW);
+  delay(100);
+}
+
+
 
